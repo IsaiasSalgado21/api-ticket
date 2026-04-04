e8f0c7d30a751b90a45eff93df53b66a321f9abe
isaias salgado <isalgado_21@alu.uabcs.mx>
2026-04-04 16:48:00 -0700

    cambios en composer.json y cors.php, preparativos para el deploy en proceso

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,15 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
-        $middleware->api(prepend: [
-            EnsureFrontendRequestsAreStateful::class,
-        ]);
-
-        $middleware->redirectGuestsTo(function () {
-            return null;
-        });
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
